d3b50888e77ac1acd17dbb710f8044f336428f46
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-11-12 11:58:06 -0500

    Set up the working space for the project

---
+++ EnhancedToDoList/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedToDoList
+//
+//  Created by Nikita Zhdanov on 2024-11-12.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
