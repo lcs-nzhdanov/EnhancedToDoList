64931e177e73d022c87243b9485cab6f9cc2c7a1
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-11-12 11:56:35 -0500

    Initial Commit

---
+++ EnhancedToDoList/EnhancedToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedToDoListApp.swift
+//  EnhancedToDoList
+//
+//  Created by Nikita Zhdanov on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnhancedToDoListTests/EnhancedToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedToDoListTests.swift
+//  EnhancedToDoListTests
+//
+//  Created by Nikita Zhdanov on 2024-11-12.
+//
+
+import Testing
+@testable import EnhancedToDoList
+
+struct EnhancedToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
